42ba736c02bc5d73be19233c8d8c3f5c753e9d0f
danielselga <danielselga@gmail.com>
2021-11-23 08:53:30 -0300

    chore: insertion sort

--- JS Algorithms and Data Structure/8-InsertionSort.js
+++ JS Algorithms and Data Structure/8-InsertionSort.js
@@ -7,3 +7,22 @@
 // Now compare the second element with the one before it and swap if necessary.
 // Continue to the next element and if it is in the incorrect order, iterate throught the sorted portion (i.e the left side) to place the element in the correct place.
 // Repeat until the array is sorted.
+
+/* INSERTION SORT: IMPLEMENTATION */
+
+function insertionSort (arr) {
+  for (let i = 1; i < arr.length; i++) {
+    console.log(arr[i])
+    const curVal = arr[i]
+    let j = 1
+    // console.log(curVal)
+    for (j = i - 1; j >= 0 && arr[j] > curVal; j--) {
+      arr[j + 1] = arr[j]
+    }
+    arr[j + 1] = curVal
+    console.log(arr)
+  }
+  return arr
+}
+
+insertionSort([2, 1, 9, 76, 4])
