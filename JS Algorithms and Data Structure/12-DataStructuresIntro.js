26eda38fbbfd8d04cd5dc744e80b67c4a68ca8ec
danielselga <danielselga@gmail.com>
2021-11-27 12:44:09 -0300

    chore: data structures intro and js classes

---
+++ JS Algorithms and Data Structure/12-DataStructuresIntro.js
@@ -0,0 +1,27 @@
+/* WHICH DATA STRUCTURE IS THE BEST */
+
+// What do they do?
+// Data structures are collections of values, the relationships among them, and the functions or operations that can be applied to the data.
+
+// Why so many???
+// Diferent data structures excel at diferent things. Some are highly specialized, while others (like arrays) are more generally used.
+
+// Why shoud care?
+// The more time you spend as a developer, the more likely you'll need to use one fo these data structures.
+// You've already worked with many of them unknowingly!
+
+// THERE IS NO ONE BEST DATA STRUCUTURE
+// They all do diferent things.
+// They all excel in diferrent situations.
+
+// Work with map/location data?
+// USE A GRAPH!
+
+// Need an ordered list with fast inserts/removals at the beginning and end?
+// USE A LINKED LINK!
+
+// Web scraping a nested HTML?
+// USE A TREE!
+
+// Need to write a scheduler?
+// USE BINARY HEAP!
