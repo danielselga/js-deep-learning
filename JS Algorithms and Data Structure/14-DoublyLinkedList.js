341dfe33c93fe063b56be785d33731c57c8ae54d
danielselga <danielselga@gmail.com>
2021-12-05 09:44:37 -0300

    chore: doubly linked list started

---
+++ JS Algorithms and Data Structure/14-DoublyLinkedList.js
@@ -0,0 +1,13 @@
+/* DOUBLY LINKED LIST */
+
+// Objectives
+// Construct a doubly linked list
+// Compare and contrast doubly linked list  and singly linked list.
+// Implement basic operations on a doubly linked list.
+
+// We know what list are... but doubly?
+// Almost identical to singly linked list, except every node has another pointer, the previous node.
+
+// Comparisons with singly linked list
+// more memory === more flexibility
+// Its almost always a tradeof.
