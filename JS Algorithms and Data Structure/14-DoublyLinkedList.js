1dd31685afe10daeadf5df34c857f7550a009184
danielselga <danielselga@gmail.com>
2021-12-05 10:37:12 -0300

    chore: push method finished and pop started

--- JS Algorithms and Data Structure/14-DoublyLinkedList.js
+++ JS Algorithms and Data Structure/14-DoublyLinkedList.js
@@ -11,3 +11,46 @@
 // Comparisons with singly linked list
 // more memory === more flexibility
 // Its almost always a tradeof.
+
+class Node {
+  constructor (val) {
+    this.val = val
+    this.next = null
+    this.prev = null
+  }
+}
+
+class DoublyLinkedList {
+  constructor () {
+    this.head = null
+    this.tail = null
+    this.length = 0
+  }
+
+  // Push pseudocode
+  // Create a new node with the value passed to the function.
+  // If the head property is null set the head and tail to be the newly created node.
+  // If not, set the next property on the tail to be that node.
+  // Set the previous property on the newly created node to be the tail.
+  // Set the tail to be the newly created node.
+  // Increment the length.
+
+  push (val) { // Push -> Adding a node to the end of the doubly linked list.
+    const newNode = new Node(val)
+    if (this.length === 0) {
+      this.head = newNode
+      this.tail = newNode
+    } else {
+      this.tail.next = newNode
+      newNode.prev = this.tail
+      this.tail = newNode
+    }
+    this.length++
+  }
+
+  // Pop pseudocode
+
+  pop () {
+
+  }
+}
