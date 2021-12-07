9398d0a06163314f191e8eeb7dacc94ec9aed07d
danielselga <danielselga@gmail.com>
2021-12-06 22:33:44 -0300

    chore: intro

---
+++ JS Algorithms and Data Structure/15-Stacks+Queue.js
@@ -0,0 +1,24 @@
+/* STACKS + QUEUE */
+
+// STACKS
+
+// OBJECTIVES
+// Define what a stack is.
+// Understand use cases for a stack.
+// Implement operations on a stack data structure.
+
+// What is a stack?
+// A LIFO data structure.
+// The last element addded to the stack will be the frist element removed from the stack.
+
+// How is it used?
+// Think about a stack of plates, or a stack of markers, o a stack of... anything.
+// As you pile it up the last thing (or the topmost thing) is what gets removed frist.
+
+// We've seen this before...
+// The call stack.
+
+// Where stacks are used
+// Managing funtion invokations.
+// Undo / Redo
+// Routing (the history object) is treated like a stack.
