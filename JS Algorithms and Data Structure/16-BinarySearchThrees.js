e6c817aecb1fc4212eafec6cbc057d8a2cddd283
Daniel Selga <danielselga@gmail.com>
2021-12-09 23:35:59 -0300

    chore: binary search three concept

---
+++ JS Algorithms and Data Structure/16-BinarySearchThrees.js
@@ -0,0 +1,32 @@
+/* BINARY SEARCH THREE */
+
+// Objectives
+// Define what a three is.
+// Compare and constrast trees and lists.
+// Explain the diferences between trees binary trees, and binary search trees.
+// Implement operations on binary search threes.
+
+// What is a three?
+// A data structure that consists of nodes in a parent / child relationship.
+
+// Three terminology
+// Root -> the top node in a three.
+// Child -> A node directly connected to another node when moving away from the root.
+// Parent -> The converse notion of a child.
+// Siblings -> A group of nodes with the same parent.
+// Leaf -> Node with no children.
+// Edge -> The connection between one node and another.
+
+// Threes -> lots of different applications
+// HTML DOM.
+// Network routing.
+// Abstract syntax threes.
+// Atificial inteligence.
+// Computer file systems.
+
+// Binary search three
+// Every parent node has at most two children.
+// Every Node to the left of a parent node is always less than the parent.
+// Every Node to the right of a parent Node is always greater than the parent.
+
+// Binary search three
