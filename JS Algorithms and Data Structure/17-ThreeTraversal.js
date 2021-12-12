09506408c5ae3a35f945802f06235d0cf617dc94
Daniel Selga <danielselga@gmail.com>
2021-12-12 11:04:50 -0300

    chore: BFS (Breath frist search) intro

--- JS Algorithms and Data Structure/17-ThreeTraversal.js
+++ JS Algorithms and Data Structure/17-ThreeTraversal.js
@@ -1,1 +1,19 @@
 /* THREE TRAVERSAL */
+
+// TRAVERSING A THREE
+// Two ways:
+// Breadth-frist search
+// Depth-frist search
+
+// Breadth-frist search intro
+
+// Steps - iteratively ->
+// Create a queue (this can be an array) and a variable to store the values of nodes visited
+// Place the root node in the queue.
+// Loop as long as there is anything in the queue ->
+// Dequeue a node from the queue and push the value of the node into the variable that stores the nodes.
+// If there is a left property on the node dequeued - add it to the queue.
+// If there is a right property on the node dequeued - add it to the queue.
+// Return the variable that stores the value.
+
+// SOLUTION INSIDE THE BINARY THREE (16 FILE).
