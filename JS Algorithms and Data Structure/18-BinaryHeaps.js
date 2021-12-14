12c232302faedba0a563158b1c0686a62f142a28
Daniel Selga <danielselga@gmail.com>
2021-12-13 21:38:05 -0300

    chore: insert pseudocode

--- JS Algorithms and Data Structure/18-BinaryHeaps.js
+++ JS Algorithms and Data Structure/18-BinaryHeaps.js
@@ -18,3 +18,13 @@
 // 2n + 1 -> left
 // Index (inside the array) * 2 + 2 -> to get the right child
 // 2n + 2 -> right
+
+// Insert pseudocode
+// Push the value into the values property on the heap.
+// Bubble the value up to its correct spot.
+// Bubble up -> 
+// Create a variable called index which is the length of the values property - 1.
+// Create a variable called parentIndex which is the floor of (index-1)/2.
+// Keep looping as long as the values element at the parentIndex is less than the values element at the child index.
+// Swap the value of the values element at the parentIndex with the value of the element propert at the child index.
+// Set the index to be the parentIndex, and start over!
