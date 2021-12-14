c5d0359ac5cedd65680e18586438a256c183a038
Daniel Selga <danielselga@gmail.com>
2021-12-13 21:45:05 -0300

    chore: maxBinaryHeap class

--- JS Algorithms and Data Structure/18-BinaryHeaps.js
+++ JS Algorithms and Data Structure/18-BinaryHeaps.js
@@ -22,9 +22,21 @@
 // Insert pseudocode
 // Push the value into the values property on the heap.
 // Bubble the value up to its correct spot.
-// Bubble up -> 
+// Bubble up ->
 // Create a variable called index which is the length of the values property - 1.
 // Create a variable called parentIndex which is the floor of (index-1)/2.
 // Keep looping as long as the values element at the parentIndex is less than the values element at the child index.
 // Swap the value of the values element at the parentIndex with the value of the element propert at the child index.
 // Set the index to be the parentIndex, and start over!
+
+class MaxBinaryHeap {
+  constructor() {
+    this.values = [];
+  }
+
+  insert(element) {
+      this.values.push(element)
+      this.bubbleUp()
+  }
+}
+
