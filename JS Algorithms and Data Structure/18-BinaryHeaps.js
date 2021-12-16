188f0b1376a564d95195522fe5b29e77a0e67063
Daniel Selga <danielselga@gmail.com>
2021-12-16 06:50:38 -0300

    chore: priority queue implementation and bigO

--- JS Algorithms and Data Structure/18-BinaryHeaps.js
+++ JS Algorithms and Data Structure/18-BinaryHeaps.js
@@ -20,114 +20,116 @@
 // 2n + 2 -> right
 
 class MaxBinaryHeap {
-	constructor() {
-		this.values = [41, 39, 33, 18, 27, 12];
-	}
+  constructor() {
+    this.values = [41, 39, 33, 18, 27, 12];
+  }
 
-	// Insert pseudocode
-	// Push the value into the values property on the heap.
-	// Bubble the value up to its correct spot.
-	// Bubble up ->
-	// Create a variable called index which is the length of the values property - 1.
-	// Create a variable called parentIndex which is the floor of (index-1)/2.
-	// Keep looping as long as the values element at the parentIndex is less than the values element at the child index.
-	// Swap the value of the values element at the parentIndex with the value of the element propert at the child index.
-	// Set the index to be the parentIndex, and start over!
+  // Insert pseudocode
+  // Push the value into the values property on the heap.
+  // Bubble the value up to its correct spot.
+  // Bubble up ->
+  // Create a variable called index which is the length of the values property - 1.
+  // Create a variable called parentIndex which is the floor of (index-1)/2.
+  // Keep looping as long as the values element at the parentIndex is less than the values element at the child index.
+  // Swap the value of the values element at the parentIndex with the value of the element propert at the child index.
+  // Set the index to be the parentIndex, and start over!
 
-	insert(element) {
-		this.values.push(element)
-		this.bubbleUp()
-	}
+  insert(val, priority) {
+    let newNode = new Node(val, priority)
+	this.values.push(newNode);
+    this.bubbleUp();
+  }
 
-	bubbleUp() {
-		let index = this.values.length - 1
-		const element = this.values[index]
+  bubbleUp() {
+    let index = this.values.length - 1;
+    const element = this.values[index];
 
-		while (index > 0) {
-			let parentIndex = Math.floor((index - 1) / 2)
-			let parent = this.values[parentIndex]
+    while (index > 0) {
+      let parentIndex = Math.floor((index - 1) / 2);
+      let parent = this.values[parentIndex];
 
-			if (element <= parent) {
-				break;
-			}
+      if (element.priority <= parent.priority) {
+        break;
+      }
 
-			this.values[parentIndex] = element
-			this.values[index] = parent
-			index = parentIndex
-		}
-	}
+      this.values[parentIndex] = element;
+      this.values[index] = parent;
+      index = parentIndex;
+    }
+  }
 
-	// Removing from a heap
-	// Remove the root
-	// Replace with the most recently added.
-	// Adjust (sink down)
+  // Removing from a heap
+  // Remove the root
+  // Replace with the most recently added.
+  // Adjust (sink down)
 
-	// Sink Down
-	// The procedure for deleting the root from the heap (effectively extracting the maximumelement in a max-heap or the minimum element in a min heap) and restore the properties is called down-heap (aka bubble-down, heapfy-down, sift-down)
+  // Sink Down
+  // The procedure for deleting the root from the heap (effectively extracting the maximumelement in a max-heap or the minimum element in a min heap) and restore the properties is called down-heap (aka bubble-down, heapfy-down, sift-down)
 
-	// Extract Max pseudocode
-	// Swap the frist value in the values property with the last one.
-	// Pop from the values property, so you can return the value at the end.
-	// Have the root "sink down" to the correct spot.
-	// Your parent index starts at 0 (the root).
-	// Find the index of the left child: 2 * index + 1 (make sure its not out of bounds).
-	// Find the index of the right child: 2 * index + 2 (make sure its not out of bounds).
-	// If the left or right child is greater than the element ... swap. If both left and right children are larger, swap with the largest child.
-	// The child index you swapped to now becomes the new parent index.
-	// Keep looping and swapping until neither child is larger than the element.
-	// Return the old root.
+  // Extract Max pseudocode
+  // Swap the frist value in the values property with the last one.
+  // Pop from the values property, so you can return the value at the end.
+  // Have the root "sink down" to the correct spot.
+  // Your parent index starts at 0 (the root).
+  // Find the index of the left child: 2 * index + 1 (make sure its not out of bounds).
+  // Find the index of the right child: 2 * index + 2 (make sure its not out of bounds).
+  // If the left or right child is greater than the element ... swap. If both left and right children are larger, swap with the largest child.
+  // The child index you swapped to now becomes the new parent index.
+  // Keep looping and swapping until neither child is larger than the element.
+  // Return the old root.
 
-	extractMax() {
-		const max = this.values[0]
-		const end = this.values.pop()
-		if (this.values.length > 0) {
-			this.values[0] = end
-			this.sinkDown()
-		}
-		return max
-	}
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return max;
+  }
 
-	sinkDown() {
-		let index = 0
-		const length = this.values.length
-		const element = this.values[0]
+  sinkDown() {
+    let index = 0;
+    const length = this.values.length;
+    const element = this.values[0];
 
-		while (true) {
-			let leftChildIndex = 2 * index + 1
-			let rigthChildIndex = 2 * index + 2
-			let leftChild, rightChild
-			let swap = null
+    while (true) {
+      let leftChildIndex = 2 * index + 1;
+      let rigthChildIndex = 2 * index + 2;
+      let leftChild, rightChild;
+      let swap = null;
 
-			if (leftChildIndex < length) {
-				leftChild = this.values[leftChildIndex]
-				if (leftChild > element) {
-					swap = leftChildIndex
-				}
-			}
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild > element) {
+          swap = leftChildIndex;
+        }
+      }
 
-			if (rigthChildIndex < length) {
-				rightChild = this.values[rigthChildIndex]
-				if (swap === null && rightChild > element ||
-					(swap !== null && rightChild > leftChild)
-				) {
-					swap = rigthChildIndex
-				}
-			}
+      if (rigthChildIndex < length) {
+        rightChild = this.values[rigthChildIndex];
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rigthChildIndex;
+        }
+      }
 
-			if (swap === null) {
-				break;
-			}
+      if (swap === null) {
+        break;
+      }
 
-			this.values[index] = this.values[swap]
-			this.values[swap] = element
-			index = swap
-		}
-	}
+      this.values[index] = this.values[swap];
+      this.values[swap] = element;
+      index = swap;
+    }
+  }
 }
 
 
-let heap = new MaxBinaryHeap()
-heap.insert(55)
+let heap = new MaxBinaryHeap();
+heap.insert(55);
 
 // PRIORITY QUEUE INTRO
 
@@ -143,15 +145,104 @@ heap.insert(55)
 
 // Val doesn't matter. Heap is constructed using prority.
 
-// Our priority queue 
+// Our priority queue
 // Write a Min Binary Heap - lower numbers means higher priority.
 // Each node has a val and a priority. Use the priority to build the heap.
 // Enqueue method accepts a value and priority, makes a new node, and puts it in the right spot based off of its priority.
 // Dequeue method removes root element, returns it and rearranges heap using priority.
 
-class priorityQueue {
-	constructor() {
-		this.values
-	}
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
 }
 
+class PriorityQueue {
+  constructor() {
+    this.values = [41, 39, 33, 18, 27, 12];
+  }
+
+  enqueue(val, priority) {
+    let newNode = new Node(val, priority);
+    this.values.push(newNode);
+    this.bubbleUp();
+  }
+
+  bubbleUp() {
+    let index = this.values.length - 1;
+    const element = this.values[index];
+
+    while (index > 0) {
+      let parentIndex = Math.floor((index - 1) / 2);
+      let parent = this.values[parentIndex];
+
+      if (element.priority <= parent.priority) {
+        break;
+      }
+
+      this.values[parentIndex] = element;
+      this.values[index] = parent;
+      index = parentIndex;
+    }
+  }
+
+
+  dequeue() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return max;
+  }
+
+  sinkDown() {
+    let index = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+
+    while (true) {
+      let leftChildIndex = 2 * index + 1;
+      let rigthChildIndex = 2 * index + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild.priority > element.priority) {
+          swap = leftChildIndex;
+        }
+      }
+
+      if (rigthChildIndex < length) {
+        rightChild = this.values[rigthChildIndex];
+        if (
+          (swap === null && rightChild.priority > element.priority) ||
+          (swap !== null && rightChild.priority > leftChild.priority)
+        ) {
+          swap = rigthChildIndex;
+        }
+      }
+
+      if (swap === null) {
+        break;
+      }
+
+      this.values[index] = this.values[swap];
+      this.values[swap] = element;
+      index = swap;
+    }
+  }
+}
+
+let ER = new PriorityQueue()
+ER.enqueue('cummon cold', 1),
+ER.enqueue('gunshot wound', 5)
+ER.enqueue('high fever', 2)
+
+// BigO of binary heaps
+// Insertion O (logN)
+// Removal O (logN)
+// Search O(N)
