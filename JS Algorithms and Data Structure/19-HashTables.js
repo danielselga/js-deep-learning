892c126969fc4e3207fd48b118a3dfeaf213af0e
Daniel Selga <danielselga@gmail.com>
2021-12-16 07:03:25 -0300

    chore: hash tables started

---
+++ JS Algorithms and Data Structure/19-HashTables.js
@@ -0,0 +1,8 @@
+/* HASH TABLES */
+
+// What is a hash table?
+// Hash tables are used to store key-value pairs. 
+// They're like arrays but the keys are not ordered.
+
+// Nerly every programming lang has some sort of hash table data structure.
+// Because of their speed, hash tables are very cummonly used.
