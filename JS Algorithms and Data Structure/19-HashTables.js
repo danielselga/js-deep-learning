c9bd590b85465dd5e0868d2b6e457dd5fbb9e4ed
Daniel Selga <danielselga@gmail.com>
2021-12-17 20:53:02 -0300

    chore: hash tables finished

--- JS Algorithms and Data Structure/19-HashTables.js
+++ JS Algorithms and Data Structure/19-HashTables.js
@@ -174,3 +174,18 @@ class HashTable {
 let ht = new HashTable();
 
 ht.set("Hello world", "goodbye");
+
+// BigO of hash tables
+
+// Insert O(1)
+// Deletion O(1)
+// Acces O(1)
+
+// Recap
+
+// Hash tables are collections of key-value pairs.
+// Hash tables can find values quickly given a key.
+// Hash tables can add new key-values quickly.
+// Hash tables store data in a large array, and work by hashing the keys.
+// A good hash should be fast, distribute keys uniformly, and be deterministic.
+// Separate chaining and linear probing are two strategies used to deal with two keys that hash to the same index.
