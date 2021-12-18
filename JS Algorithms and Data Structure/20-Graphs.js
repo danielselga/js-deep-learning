36b035f8d13f395b7db4c0481f6a7f57effb6ed1
Daniel Selga <danielselga@gmail.com>
2021-12-18 11:51:35 -0300

    chore: adjacency matrix and adjacency list

--- JS Algorithms and Data Structure/20-Graphs.js
+++ JS Algorithms and Data Structure/20-Graphs.js
@@ -28,3 +28,14 @@
 // Weighted/Unwheighted -> values assigned to distances between vertices
 // Directed / Undirected -> directions assigned to distanced between vertices
 
+// Adjacency Matrix
+
+// Is a square matrix used to represent a finite graph.
+// The elements of the matrix indicate wheter pairs of vertices are adjacent or not in the graph.
+
+// Adjacency List
+
+// Is a collection of unordered lists used to represent a finite graph. 
+// Each unordered list within an adjacency list describes the set of neighbors of a particular vertex in the graph. This is one of several commonly used representations of graphs for use in computer programs.
+
+
