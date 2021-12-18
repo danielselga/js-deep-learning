d6ddd7df999088b6c296b9a01384af5d5be187d1
Daniel Selga <danielselga@gmail.com>
2021-12-17 21:01:03 -0300

    chore: graphs started

---
+++ JS Algorithms and Data Structure/20-Graphs.js
@@ -0,0 +1,13 @@
+/* GRAPHS */
+
+// Objectives
+
+// Explain what is a graph
+// Compare and contrast differents types of graphs and their use cases in the real world
+// Implement a graph list 
+// Traverse through a graph using BFS and DFS
+// Compare and contrast graph traversal algorithms
+
+// What are graphs?
+
+// A graph data strucuture consists of a finite (and possibly mutable) set of vertices or nodes or points, together with a set of unordered pairs of these vertices for an undirected graph or a set of ordered pairs for directed graphs.
