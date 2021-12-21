1e4d24380b54c8a27dbe6ee7fbdedec5e61197c4
Daniel Selga <danielselga@gmail.com>
2021-12-20 23:01:24 -0300

    chore: dijkstras objectives

--- JS Algorithms and Data Structure/22-DijkstrasAlgo.js
+++ JS Algorithms and Data Structure/22-DijkstrasAlgo.js
@@ -1,1 +1,7 @@
 /* DIJKSTRAS ALGORITHM */
+
+// Objectives
+// Understand the importance of Dijkstras
+// Implement a weighted graph
+// Implment Dijkstras using a naive priority queue
+// Implement Dijkstras using a binary heap priority queue
