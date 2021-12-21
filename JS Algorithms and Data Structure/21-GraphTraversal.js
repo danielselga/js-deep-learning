239813afed431322ca927a5453a75a30de114f45
Daniel Selga <danielselga@gmail.com>
2021-12-20 21:14:40 -0300

    chore: depth first search pseudocode

--- JS Algorithms and Data Structure/21-GraphTraversal.js
+++ JS Algorithms and Data Structure/21-GraphTraversal.js
@@ -27,3 +27,13 @@
     recursively call DFS on neighbor
 } */
 
+// DEPTH FRIST TRAVERSAL (DETAILED PSEUDOCODE)
+
+// The function should accept a starting node
+// Create a list to store the end result, to be returned at the very end
+// Create an object to store your visited vertices
+// Create a helper function which accepts a vertex
+    // The helper function should return early if the vertex is empty
+    // The helper function should place the vertex it accepts into the visited object and push that vertex into the result array
+    // Loop over all of the values in the adjacencyList for that vertex
+    // If any of those values have not been visited recursively invoke the helper function with that vertex
