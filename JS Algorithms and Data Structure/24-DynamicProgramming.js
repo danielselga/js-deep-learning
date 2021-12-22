f100dcb95ba932b1912d9ded23fff759df0277b3
Daniel Selga <danielselga@gmail.com>
2021-12-22 20:34:30 -0300

    chore: dynamic programing intro

---
+++ JS Algorithms and Data Structure/24-DynamicProgramming.js
@@ -0,0 +1,10 @@
+/* DYNAMIC PROGRAMMING */
+
+// Objectives
+// Define what dynamic programming is
+// Explain what overlapping subproblems are
+// Understand what optimal substructure is
+// Solve more challenging problems using dynamic programming
+
+// WTF IS DYNAMIC PROGRAMMIN
+// A method for solving a complex problem by breaking it down into a collection of simpler subproblems, solving each of those subproblems just once, and storing their solution.
