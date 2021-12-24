0a5c11edefa453059bb07dfaf61408ee21f7e198
Daniel Selga <danielselga@gmail.com>
2021-12-24 14:59:07 -0300

    chore: overlapping solutions

--- JS Algorithms and Data Structure/24-DynamicProgramming.js
+++ JS Algorithms and Data Structure/24-DynamicProgramming.js
@@ -8,3 +8,11 @@
 
 // WTF IS DYNAMIC PROGRAMMIN
 // A method for solving a complex problem by breaking it down into a collection of simpler subproblems, solving each of those subproblems just once, and storing their solution.
+
+// It only works on problems with optimal substructure and overlaping subproblems.
+
+// Overlaping subproblems
+// A problem is said have overlapping subproblems if it can be broken down into subproblems which are reused several times
+
+// Optimal substructure
+// A problem is said to have optimal substructure if an optimal solution can be constructed form optimal solutions of its subproblems
