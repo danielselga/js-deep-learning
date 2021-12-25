da46399f3b031ade51ed4793100cdd3ec8443a10
Daniel Selga <danielselga@gmail.com>
2021-12-25 17:59:01 -0300

    chore: overlapping solutions

--- JS Algorithms and Data Structure/24-DynamicProgramming.js
+++ JS Algorithms and Data Structure/24-DynamicProgramming.js
@@ -16,3 +16,56 @@
 
 // Optimal substructure
 // A problem is said to have optimal substructure if an optimal solution can be constructed form optimal solutions of its subproblems
+
+// Recursive solution for fibonati 
+
+function fib(n) {
+    if (n <= 2) {
+        return 1
+    }
+
+    return fib(n-1) + fib (n-2)
+}
+
+// Memoization
+// Storing the results of expensive function calls and returning the cached result when the same inputs occour again.
+
+// A memo-ized solution
+
+function fib(n, memo = []) {
+    if (memo[n] !== undefined) {
+        return memo[n]
+    }
+
+    if (n <= 2) {
+        return 1
+    }
+
+    const res = fib(n-1, memo) + fib(n-2, memo)
+    memo[n] = res
+    return res
+}
+
+// Tabulation
+// Storing the result of a previous result in a table (usualy an array)
+
+// Usualy done using iteration
+
+// Better space complexity can be achived using tabulation
+
+// A tabulation solution
+
+function fib(n) {
+    if (n <= 2) {
+        return 1
+    }
+
+    const fibNums = [0, 1, 1]
+
+    for (let i = 3; i <= n; i++) {
+        fibNums[i] = fibNums[i - 1] + fibNums[i - 2]
+    }
+
+    return fibNums[n]
+}
+
