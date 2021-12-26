b3af0b6a7cfed5dd7fab2af43933f95c5f0ebbf2
Daniel Selga <danielselga@gmail.com>
2021-12-26 12:06:58 -0300

    chore: Specifying a range in character sets

--- Regex/CharacterSets.js
+++ Regex/CharacterSets.js
@@ -22,3 +22,30 @@ let re = /[rh][abcd][ i]/ // All 'd ' (d spaces) will match.
 
 // Matching rules
 let re = /[1234]/ // If we have all of these characters in our value will match 4 times.
+
+// Specifying a range
+
+//  1 to 4
+let re = /[1-4]/g
+
+// Special characters range
+let re = /[\-.]/
+
+// Range of letters
+let re = /[a-e]/
+
+// Combining number sets
+let re = /[1-4][1-4]/ // This only match 2 numbers together ex: 42
+
+// Upercase matching
+let re = /[A-Z]/g
+
+// Scaping ranges (ignore the hifen)
+let re = /12\-5/g
+
+// Each character set matchs a single character
+
+// In sets matching we need to think a single numbler is a single set 
+
+// Not work
+let nt = /[10-20]/g
