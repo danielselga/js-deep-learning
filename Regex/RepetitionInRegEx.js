45fb0467e58ab07a1c5746f7186b4c8352962bb6
Daniel Selga <danielselga@gmail.com>
2021-12-28 01:42:38 -0300

    chore: character repetition started

---
+++ Regex/RepetitionInRegEx.js
@@ -0,0 +1,7 @@
+// Indicating character repetition
+// + Matches one or more occurrences
+// ? Matches zero or one occurrences
+// * Matches zero or more occurences -> Modify the last character (desconsiderate)
+
+// Repetition usualy get the entire word, they go to the end and repeat as much they can.
+
