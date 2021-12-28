f692934d5d5878239accafd4b21d1f6590665005
Daniel Selga <danielselga@gmail.com>
2021-12-28 01:52:28 -0300

    chore: repetition lazy and greedy

--- Regex/RepetitionInRegEx.js
+++ Regex/RepetitionInRegEx.js
@@ -1,7 +1,10 @@
 // Indicating character repetition
 // + Matches one or more occurrences
-// ? Matches zero or one occurrences
+// ? Matches zero or one occurrences -> Make the regex lazy (matches less as possible)
 // * Matches zero or more occurences -> Modify the last character (desconsiderate)
 
-// Repetition usualy get the entire word, they go to the end and repeat as much they can.
+// By default regex are greedy they go as many they can.
+
+
+
 
