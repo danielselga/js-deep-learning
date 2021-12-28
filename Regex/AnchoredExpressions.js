120219872b629da6ff2cdf7fa45c6f080ceeca09
Daniel Selga <danielselga@gmail.com>
2021-12-28 11:58:12 -0300

    chore: word boundary

---
+++ Regex/AnchoredExpressions.js
@@ -0,0 +1,9 @@
+// Anchored expressions
+
+// Anchoring metacharacters
+
+// ^ Anchors the match to the start of the line
+// $ Anchors the match to the end of the line
+
+// Word boundary
+// \b Word Boundary - Pattern bounded by a non-word character
