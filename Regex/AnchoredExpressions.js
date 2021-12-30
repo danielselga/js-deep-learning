8f1a84167ac640b9ab3990f4f7c45cb2d6ee334d
Daniel Selga <danielselga@gmail.com>
2021-12-30 11:58:40 -0300

    chore: anchored expressions finished

--- Regex/AnchoredExpressions.js
+++ Regex/AnchoredExpressions.js
@@ -12,3 +12,26 @@
 // References position, not an actual character.
 // Word characters: \w or [a-zA-Z0-9_] -> Underscore is a part of a word character
 
+// Anchored only will match 5 digits
+let re = /^[0-9]{5}$/
+
+// Data set and achor combo to get USA zip code
+let re = /^[0-9]{5}-[0-9]{4}$/
+
+// Acurate regex rules
+// When possible, define the quantity of repeated expressions
+// Narrow the scope of repeated expressions
+// Provide clear starting and ending points
+
+// Exercise
+
+let text1 = `Each and every Tuesday, at the beginning of the day, we hold a staff meeting. 
+At the Tuesday staff meeting, you will need to make a report on the past weeks progress, 
+and you will receive assignments for the following Tuesday. Just be aware that somedays this 
+Tuesday meeting might not occur. When that happens, we will make an announcement.`
+
+let reg = /\b[mtwfs][a-z]{1,4}[nsir]\b/gi
+
+let newText = text1.replace(reg, 'Monday')
+
+console.log(newText)
