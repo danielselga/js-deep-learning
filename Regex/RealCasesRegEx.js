dc68ad30a376e59500f48d838d60e6709a0c63b0
Daniel Selga <danielselga@gmail.com>
2021-12-31 19:28:45 -0300

    chore: email .com.br regex

--- Regex/RealCasesRegEx.js
+++ Regex/RealCasesRegEx.js
@@ -8,3 +8,7 @@ let regEmail = /.+@[a-z]+\.com/gi // Simple email
 
 let regEmail2 = /^[^\s@]+@[^\s@.]+\.com$/ // More complete case, using ignored sets [^] to dont match spaces and @, more strict.
 
+let regEmail3 = /^[^\s@]+@[^\s@.]+\.[^\s@.]+[^\s@]+$/ // PT-BR Case, if the user have .com.something this match too.
+
+// Matching a twitter handler
+
