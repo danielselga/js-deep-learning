d200efc3eb668c11dade6df9fb591c2d6ed4c9b8
Daniel Selga <danielselga@gmail.com>
2021-12-31 19:46:26 -0300

    chore: regex test for password by me

--- Regex/RealCasesRegEx.js
+++ Regex/RealCasesRegEx.js
@@ -4,14 +4,39 @@
 
 const email = 'danielselga@gmail.com'
 
-let regEmail = /.+@[a-z]+\.com/gi // Simple email
+const regEmail = /.+@[a-z]+\.com/gi // Simple email
 
-let regEmail2 = /^[^\s@]+@[^\s@.]+\.com$/ // More complete case, using ignored sets [^] to dont match spaces and @, more strict.
+const regEmail2 = /^[^\s@]+@[^\s@.]+\.com$/ // More compconste case, using ignored sets [^] to dont match spaces and @, more strict.
 
-let regEmail3 = /^[^\s@]+@[^\s@.]+\.[^\s@.]+[^\s@]+$/ // PT-BR Case, if the user have .com.something this match too.
+const regEmail3 = /^[^\s@]+@[^\s@.]+\.[^\s@.]+[^\s@]+$/ // PT-BR Case, if the user have .com.something this match too.
 
 // Matching a twitter name
 
-let twitterName = '@danielselgadev'
+const twitterName = '@danielselgadev'
 
-let regTwitter = /^@?(\w+)$/ // Using capturing grups for easily store in js
+const regTwitter = /^@?(\w+)$/ // Using capturing grups for easily store in js
+
+// Testing passwords
+
+const password = 'N2RN3/<zvd'
+
+const theLenght = /^.{8,32}$/
+const upper = /[A-Z]/
+const lower = /[a-z]/
+const numbers = /[0-9]/
+const special = /[" !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]/
+
+const rules = [upper, lower, numbers, special]
+
+function testPassword(pass) {
+    rules.forEach(rules => {
+        if(rules.test(pass)) {
+            console.log(rules, 'passed')
+        } else {
+            console.log(rules, 'reproved')
+        }
+        
+    })
+}
+
+testPassword(password)
