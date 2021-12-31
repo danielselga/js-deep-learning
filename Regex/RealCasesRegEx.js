d530aa44deb6a82fc412c5fb3adba03d4182b5f5
Daniel Selga <danielselga@gmail.com>
2021-12-31 19:24:10 -0300

    chore: real cases regex

---
+++ Regex/RealCasesRegEx.js
@@ -0,0 +1,7 @@
+// Matching an email address
+
+const email = 'danielselga@gmail.com'
+
+let regEmail = /.+@[a-z]+\.com/gi // Simple email
+
+let regEmail2 = /^[^\s@]+@[^\s@.]+\.com$/ // More complete case, using ignored sets [^] to dont match spaces and @, more strict.
